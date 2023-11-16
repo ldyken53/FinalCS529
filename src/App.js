e3116635113da9ed1808668b3438105f70c2ad26
Landon Dyken <ldyken53@uab.edu>
2023-11-16 01:21:47 -0600

    Remove unnecessary and clean code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { useState, useEffect, useMemo, useRef } from 'react';
 import './App.css';
 
-import Particle3D from './Particle3D';
-import LinkedViewD3 from './LinkedViewD3';
-import ColorLegend from './ColorLegend';
-import EmptyView from './EmptyView';
 import ColorPicker from './ColorPicker';
 import GradientBar from './GradientBar';
 
@@ -15,13 +11,6 @@ import * as tiff from 'tiff'
 import Panzoom from '@panzoom/panzoom'
 
 function App() {
-
-  //data
-  const [particleData, setParticleData] = useState();
-  //states for brushing and linking
-  const [brushedCoord, setbrushedCoord] = useState(0);
-  const [brushedAxis, setBrushedAxis] = useState('x');
-
   const [colorL5, setColorL5] = useState('#c6dbef');
   const [colorL6, setColorL6] = useState('#084594');
 
@@ -77,17 +66,6 @@ function App() {
   var canvasRefL6 = useRef(null);
   var hiddenRef = useRef(null);
   var dataFetched = false;
-
-  //TODO: Edit this to change the extents of the color scale for the linked view
-  //saturated color scale used to color the particles
-  const colorRange = ['#c6dbef', '#084594'];
-
-  //how wide the 'brushed' area should be
-  const brushedAreaThickness = .5;
-
-  //TODO: for extra credit, set this to false if you want to allow the cross-section plane (brushedAxis) to be toggleable in the Particle3D View
-  //otherwise, fix the value to 'x' so the code in LinkedViewD3 doesn't break
-  const allowAxisToggle = true;
 
   function imshow(data, pixelSize, color, canvasRef, scale) {
     // Flatten 2D input array
@@ -171,33 +149,6 @@ function App() {
     )
   }
 
-  //wrapper for updating the axis we're slicing through for brushing 
-  //reset the coordinate on a toggle
-  function toggleBrushedAxis(coord) {
-    if (allowAxisToggle & brushedCoord !== coord) {
-      setBrushedAxis(coord);
-      setbrushedCoord(0);
-    }
-  }
-
-  //wrapper for increasing brush coordinate position on events
-  function incrementBrush(scale) {
-    let newY = brushedCoord + brushedAreaThickness * scale;
-    setbrushedCoord(newY);
-  }
-
-  //event handler for key presses (up/down for incrementing brush event)
-  function handleKeyPress(e) {
-    console.log('e', e)
-    e.preventDefault();
-    if (e.keyCode === 38) {
-      incrementBrush(.1)
-    } else if (e.keyCode === 40) {
-      incrementBrush(-.1);
-    }
-  }
-
-  //fetch the dta 
   useEffect(() => {
     if (!dataFetched) {
       fetchData();
@@ -205,60 +156,8 @@ function App() {
     dataFetched = true;
   }, []);
 
-  //calculate the extents of the datapoints once
-  function getBounds(data) {
-    if (data === undefined) {
-      return
-    }
-    var bounds = {};
-    data.forEach(function (d) {
-      // get the min bounds
-      const p = d.position
-      bounds.minX = Math.min(bounds.minX || Infinity, p[0]);
-      bounds.minY = Math.min(bounds.minY || Infinity, p[2]);
-      bounds.minZ = Math.min(bounds.minZ || Infinity, p[1]);
-
-      // get the max bounds
-      bounds.maxX = Math.max(bounds.maxX || -Infinity, p[0]);
-      bounds.maxY = Math.max(bounds.maxY || -Infinity, p[2]);
-      bounds.maxZ = Math.max(bounds.maxZ || -Infinity, p[1]);
-
-      bounds.maxC = Math.max(bounds.maxC || -Infinity, d.concentration);
-    });
-    return bounds
-  }
-
-
-  //calculate bounds of the data once loaded
-  const bounds = useMemo(() => {
-    return getBounds(particleData);
-  }, [particleData])
-
-
-  //conditional accessor for the data feature we're using the brush the data
-  function getBrushedCoord(d) {
-    if (brushedAxis === 'y') {
-      return d.position[2] - ((bounds.maxY - bounds.minY) / 2)
-    } else if (brushedAxis === 'z') {
-      return d.position[1]
-    }
-    return d.position[0]
-  }
-
-  //make buttons that toggle which axis we're using to slice the data
-  const axisToggles = ['x', 'y', 'z'].map(c => {
-    let active = c === brushedAxis;
-    let variant = active ? 'activeButton' : 'inactiveButton';
-    return (<button
-      onClick={() => toggleBrushedAxis(c)}
-      className={variant}
-    >{c}</button>)
-  })
-
-  //tabIndex is needed to put the keypress event on the div
   return (
     <div
-      onKeyUp={handleKeyPress}
       tabIndex={0}
       className="App" style={{ 'height': '100vh', 'width': '100vw' }}
     >
@@ -273,57 +172,6 @@ function App() {
           <canvas width={512} height={512} ref={canvasRefL6}/>
         </div>
         <canvas hidden={true} width={2048} height={2048} ref={hiddenRef}/>
-      </div>
-      {/* <div 
-        className={'shadow'}
-        style={{'height':'20vw','width':'calc(49vw - 10em)','maxHeight':'80vh','display':'inline-block','margin':'3px'}}
-      >
-        <EmptyView
-          colorRange={colorRange}
-          bounds={bounds}
-          data={particleData}
-          brushedCoord={brushedCoord}
-          brushedAreaThickness={brushedAreaThickness}
-          brushedAxis={brushedAxis}
-          getBrushedCoord={getBrushedCoord}
-        />
-        <div style={{'width':'100%','fontSize':'1.5em'}}>
-            {'L5 Cell Expression'}
-        </div>
-      </div> */}
-      {/* <div 
-        className={'shadow'}
-        style={{'height':'20vw','width':'calc(48vw - 10em)','maxHeight':'80vh','display':'inline-block','margin':'3px'}}
-      >
-        <EmptyView
-          colorRange={colorRange}
-          bounds={bounds}
-          data={particleData}
-          brushedCoord={brushedCoord}
-          brushedAreaThickness={brushedAreaThickness}
-          brushedAxis={brushedAxis}
-          getBrushedCoord={getBrushedCoord}
-        />
-        <div style={{'width':'100%','fontSize':'1.5em'}}>
-            {'L6 Cell Expression'}
-        </div>
-      </div> */}
-      <div
-        className={'shadow'}
-        style={{ 'height': '20vw', 'width': 'calc(49vw - 10em)', 'maxHeight': '80vh', 'display': 'inline-block', 'margin': '3px' }}
-      >
-        <EmptyView
-          colorRange={colorRange}
-          bounds={bounds}
-          data={particleData}
-          brushedCoord={brushedCoord}
-          brushedAreaThickness={brushedAreaThickness}
-          brushedAxis={brushedAxis}
-          getBrushedCoord={getBrushedCoord}
-        />
-        <div style={{ 'width': '100%', 'fontSize': '1.5em' }}>
-          {'Overlay View'}
-        </div>
       </div>
       <div
         style={{ 'height': '50vw', 'width': '20em', 'maxHeight': '80vh', 'display': 'inline-block', 'marginTop': '0px' }}
